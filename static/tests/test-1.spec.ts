a1c02f9b927edbfdf657b13d0553159575d47863
alessandro trinca tornidor <alessandro@trinca.tornidor.com>
2024-11-15 20:35:59 +0100

    test: add first playwright test e2e

---
+++ static/tests/test-1.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect, chromium } from "@playwright/test";
+
+test("test: get a custom sample writing within the input field.", async () => {
+  const browser = await chromium.launch({
+    args: [
+      "--use-fake-device-for-media-stream",
+      "--use-fake-ui-for-media-stream",
+    ],
+  });
+  const context = await browser.newContext();
+  context.grantPermissions(["microphone"]);
+  const page = await browser.newPage({});
+  await page.goto("/");
+
+  const inputField = page.getByPlaceholder(
+    "Write and press enter to filter"
+  );
+  await inputField.fill("Hi Tom, how are you?");
+  await inputField.press("Enter");
+  await expect(page.getByText("Hi Tom, how are you?")).toBeVisible();
+  await expect(
+    page.getByText("/ hiː toːm, hoː aːrɛː yːuː? /")
+  ).toBeVisible();
+  console.log("end");
+  await page.close();
+});
