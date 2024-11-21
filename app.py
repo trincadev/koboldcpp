d51ffe7d753521555e7dc27d47b4382d6988a221
alessandro trinca tornidor <alessandro@trinca.tornidor.com>
2024-11-21 23:00:06 +0100

    feat: update the example section, add a random choice for the phrase

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
 from aip_trainer import app_logger
-from aip_trainer.lambdas import lambdaSpeechToScore, lambdaTTS
+from aip_trainer.lambdas import lambdaGetSample, lambdaSpeechToScore, lambdaTTS
 
 
 js = """
@@ -28,6 +28,12 @@ function updateCssText(text, letters) {
 }
 """
 
+
+def change_interactivity_components(components, is_active: bool):
+    for component in components:
+        component.interactive = is_active
+
+
 with gr.Blocks() as gradio_app:
     app_logger.info("start gradio app building...")
 
@@ -42,8 +48,22 @@ with gr.Blocks() as gradio_app:
     with gr.Row():
         with gr.Column(scale=4, min_width=300):
             with gr.Row():
-                with gr.Column(scale=1, min_width=50):
+                with gr.Column(scale=2, min_width=80):
                     language = gr.Radio(["de", "en"], label="Language", value="en")
+                with gr.Column(scale=5, min_width=160):
+                    difficulty = gr.Radio(
+                        label="Difficulty",
+                        value=0,
+                        choices=[
+                            ("random", 0),
+                            ("easy", 1),
+                            ("medium", 2),
+                            ("hard", 3),
+                        ],
+                    )
+                with gr.Column(scale=1, min_width=100):
+                    btn_random_phrase = gr.Button(value="Choose a random phrase")
+            with gr.Row():
                 with gr.Column(scale=7, min_width=300):
                     learner_transcription = gr.Textbox(
                         lines=3,
@@ -51,27 +71,29 @@ with gr.Blocks() as gradio_app:
                         value="Hi there, how are you?",
                     )
             with gr.Row():
+                with gr.Column(scale=7, min_width=240):
+                    tts = gr.Audio(label="Audio TTS")
+                with gr.Column(scale=1, min_width=50):
+                    btn_tts = gr.Button(value="Run TTS")
+            with gr.Row():
                 learner_recording = gr.Audio(
                     label="Learner Recording",
                     sources=["microphone", "upload"],
                     type="filepath",
                 )
-            with gr.Row():
-                tts = gr.Audio(label="tts")
-                btn = gr.Button(value="TTS")
-                btn.click(
-                    fn=lambdaTTS.get_tts,
-                    inputs=[learner_transcription, language],
-                    outputs=tts,
-                )
-            gr.Examples(
+        with gr.Column(scale=4, min_width=320):
+            examples_text = gr.Examples(
                 examples=[
-                    ["Hi there, how are you?", "en"],
-                    ["Hallo, wie geht es dir?", "de"],
+                    ["Hi there, how are you?", "en", 1],
+                    ["Hallo, wie geht es dir?", "de", 1],
+                    ["Die König-Ludwig-Eiche ist ein Naturdenkmal im Staatsbad Brückenau.", "de", 2,],
+                    ["Rome is home to some of the most beautiful monuments in the world.", "en", 2],
+                    ["Some machine learning models are designed to understand and generate human-like text based on the input they receive.", "en", 3],
+                    ["Die König-Ludwig-Eiche ist ein Naturdenkmal im Staatsbad Brückenau, einem Ortsteil des drei Kilometer nordöstlich gelegenen Bad Brückenau im Landkreis Bad Kissingen in Bayern.", "de", 3],
                 ],
-                inputs=[learner_transcription, language],
+                inputs=[learner_transcription, language, difficulty],
             )
-        with gr.Column(scale=3, min_width=300):
+
             transcripted_text = gr.Textbox(
                 lines=2, placeholder=None, label="Transcripted text", visible=False
             )
@@ -100,9 +122,13 @@ with gr.Blocks() as gradio_app:
                 value=" - ",
                 elem_classes="speech-output",
             )
-            btn = gr.Button(value="Recognize speech accuracy")
-            # real_transcripts, is_letter_correct_all_words, pronunciation_accuracy, result['recording_ipa'], real_transcripts_ipa, res
-
+            with gr.Row():
+                btn = gr.Button(value="Recognize speech accuracy")
+            btn_random_phrase.click(
+                lambdaGetSample.get_random_selection,
+                inputs=[language, difficulty],
+                outputs=[learner_transcription],
+            )
     btn.click(
         lambdaSpeechToScore.get_speech_to_score_tuple,
         inputs=[learner_transcription, learner_recording, language],
@@ -114,6 +140,11 @@ with gr.Blocks() as gradio_app:
             ideal_ipa,
             res,
         ],
+    )
+    btn_tts.click(
+        fn=lambdaTTS.get_tts,
+        inputs=[learner_transcription, language],
+        outputs=tts,
     )
     html_output.change(
         None,
